607919d4c4ea7a60eb8947e6c7274d55ad868397
root <root@server>
2017-01-18 15:38:05 +0100

    install prog stuff updated

---
+++ 310-installProgrammingStuff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+## C++ - Programming - Stuff
+apt-get -y install g++ jedit geany kate vim gedit
+
+###############################
+## Java - Programming Stuff
+apt-get -y install openjdk-7-jdk
+
+## Scratch
+apt-get -y install scratch
+
+##BlueJ
+
+##greenfoot 
+
+########################END
+
+## Free Pascal
+apt-get -y install fpc
